6242947505043851855dd75e2060c53cd4b4ec8f
xt <1799250230@qq.com>
2022-09-09 00:17:49 +0800

    feat#second commit

--- study/src/main/java/com/xt/demo/Demo1.java
+++ study/src/main/java/com/xt/demo/Demo1.java
@@ -7,12 +7,13 @@ package com.xt.demo;
 public class Demo1 {
     public static void main(String[] args) {
         test();
-        System.out.println("worffffffld");
+        System.out.println("w11111111");
     }
-
     public static void test() {
         for (int i = 0; i < 7; i++) {
-            System.out.println("make===== some noise ");
+            System.out.println("dev 分支最新已提交到远程  make===== some noise ");
+            System.out.println("dev 分支最新已提交到远程  make===== some noise ");
+            System.out.println("dev 分支最新已提交到远程  make===== some noise ");
         }
     }
 
