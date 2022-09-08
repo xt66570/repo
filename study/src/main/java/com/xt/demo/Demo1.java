42b02c64cbd0c5ed954f723c24175c912dfd7950
xt <1799250230@qq.com>
2022-09-09 01:15:39 +0800

    feat#5 commit

--- study/src/main/java/com/xt/demo/Demo1.java
+++ study/src/main/java/com/xt/demo/Demo1.java
@@ -7,11 +7,20 @@ package com.xt.demo;
 public class Demo1 {
     public static void main(String[] args) {
         test();
+        test();
+        test();
+        test();
+        System.out.println("worffffffld");
+        System.out.println("worffffffld");
+        System.out.println("worffffffld");
         System.out.println("worffffffld");
     }
 
     public static void test() {
         for (int i = 0; i < 7; i++) {
+            System.out.println("make===== some noise ");
+            System.out.println("make===== some noise ");
+            System.out.println("make===== some noise ");
             System.out.println("make===== some noise ");
         }
     }
