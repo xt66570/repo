89ce6a388460b28f7d68dedf508427cb86d9ede9
xt <1799250230@qq.com>
2022-09-08 22:23:43 +0800

    feat#first commit

---
+++ study/src/main/java/com/xt/demo/Demo1.java
@@ -0,0 +1,19 @@
+package com.xt.demo;
+
+/**
+ * @author XT
+ * @create 2022/9/8 22:17
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        test();
+        System.out.println("hello world");
+    }
+
+    public static void test() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("那就这样吧");
+        }
+    }
+
+}
